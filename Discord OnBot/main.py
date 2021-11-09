ea5aa44ea20f28e669180397bfeb67034627140c
Programmer420-1 <limweixin17@gmail.com>
2021-11-09 09:17:39 +0800

    Multiserver enabled

--- Discord OnBot/main.py
+++ Discord OnBot/main.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-import music_updated
+from music_updated import music_updated
 #from server import keep_alive
 
 cogs  = [music_updated]
@@ -8,8 +8,14 @@ cogs  = [music_updated]
 client = commands.Bot(command_prefix = "//", intents = discord.Intents.all())
 client.remove_command('help')
 
-for i in range(len(cogs)):
-  cogs[i].setup(client)
+@client.event
+async def on_ready():
+  print(f"{client.user.name} is ready")
 
+async def setup(): # wait the bot is ready only we add the cog , otherwise self.guild.id will return none
+  await client.wait_until_ready()
+  client.add_cog(music_updated(client))
+
+client.loop.create_task(setup())
 #keep_alive()
 client.run("ODk0MDkxMTQ5MDc2MjA1NTg4.YVk9RQ.jB2mmKt3S4E8H7AMNMxlWEHvGD0")
--- Discord OnBot/music_updated.py
+++ Discord OnBot/music_updated.py
@@ -2,7 +2,8 @@ import discord
 from discord.ext import commands
 import youtube_dl
 import pafy
-
+import threading
+import time
 
 command_list = [
   '//help => Get help on the list of commands and its functionality',
@@ -10,53 +11,86 @@ command_list = [
   '//play [song_name] => Start playing songs in queue',
   '//pause => Pause the player',
   '//resume => Resume the player',
+  '//skip => Skip to the next song in queue',
   '//remove [index] => Remove specific songs from queue',
   '//queue => Display a list of songs which are currenly in queue',
   '//disconnect => Disconnect On Bot from the voice channel'
 ]
 
+queue = {}
+nowPlaying = {}
+
 class music_updated(commands.Cog):
-  song_list = []
   nextSong = False
-  now_playing = None
   FFMPEG_OPTIONS = {
       'before_options' : '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
       'options' : '-vn'
       }
   YDL_OPTIONS = {
     'format' : 'bestaudio',
-    'quite': True,
+    'quiet': True,
+    'forcetitle' : True,
   }
 
+  ## constructor
   def __init__(self,client):
+    print("Setting up bot")
     self.client = client
-  
+    self.Setup()
+
+    # start new thread for checking new servers added [Always running until program terminated]
+    t = threading.Thread(target=self.checkNewServer, daemon=True)
+    t.start()
+
+  ## bot set up
+  def Setup(self):
+    global queue
+    global nowPlaying
+    print("running set up method")
+    for guild in self.client.guilds:
+      print(str(guild.id)+" << guild id")
+      queue[guild.id] = []
+      nowPlaying[guild.id] = None
+
+  # check server which bot is in every 0.5s
+  def checkNewServer(self):
+    global queue
+    global nowPlaying
+    new_guild_list = []
+    while True:
+      for guild in self.client.guilds:
+        new_guild_list.append(guild.id)
+      current_guild_list = list(queue.keys())
+
+      if new_guild_list[-1] not in current_guild_list and len(current_guild_list) < len(new_guild_list):
+        queue[new_guild_list[-1]] = []
+        nowPlaying[new_guild_list[-1]] = None
+        print(queue.keys())
+      time.sleep(0.5)
+
   ## check songs in queue
   async def check_queue(self,ctx):
+      global queue
+      global nowPlaying
       ## if there is songs in queue
-      if len(self.song_list) > 0:
+      if len(queue[ctx.guild.id]) > 0:
         ## stop the player first
         ctx.voice_client.stop()
         ## play next songs in queue
-        url,self.now_playing =  self.song_list[0].split("#+-+#")[1],self.song_list[0].split("#+-+#")[0]
-        del self.song_list[0]
-        await self.play_song(ctx,url,self.now_playing)
+        url,nowPlaying[ctx.guild.id] =  queue[ctx.guild.id][0].split("#+-+#")[1],queue[ctx.guild.id][0].split("#+-+#")[0]
+        del queue[ctx.guild.id][0]
+        await self.play_song(ctx,url,nowPlaying[ctx.guild.id])
       ## update nowplaying info after playing last song
       else:
-        self.now_playing = None
+        nowPlaying[ctx.guild.id] = None
 
   async def search(self, ctx, song=None):
     ## retrieve info of songs by song title
-    #info = await self.client.loop.run_in_executor(None,lambda : YoutubeSearch(song, max_results=1).to_dict())
     info = await self.client.loop.run_in_executor(None,lambda : youtube_dl.YoutubeDL(self.YDL_OPTIONS).extract_info(f"ytsearch:{song}",download = False,ie_key = "YoutubeSearch"))
     if len(info["entries"]) == 0:
       return None
     else:
       return [entry["webpage_url"] for entry in info["entries"]]
-      #id = info[0]['id']
-      #with youtube_dl.YoutubeDL(self.YDL_OPTIONS) as ydl:
-      #  infos = ydl.extract_info(f"ytsearch:{id}", download = False)
-      #  return infos
   
   async def play_song(self,ctx,song,song_name = None):
     ## takes in url as argument
@@ -71,7 +105,6 @@ class music_updated(commands.Cog):
   ## join voice channel
   @commands.command()
   async def join(self,ctx):
-    #if user is not in voice channel
     if ctx.author.voice is None:
       await ctx.send("You are not in a voice channel!")
     voice_channel = ctx.author.voice.channel
@@ -81,12 +114,16 @@ class music_updated(commands.Cog):
     #not a voice channel -> move over to a voice channel
     else:
       await ctx.voice_client.move_to(voice_channel)
+    
   
   ## disconnect bot from voice channel
   @commands.command()
   async def disconnect(self,ctx):
+    global queue
+    global nowPlaying
     try:
-      self.song_list = []
+      queue[ctx.guild.id] = []
+      nowPlaying[ctx.guild.id] = None
       ctx.voice_client.stop()
       await ctx.voice_client.disconnect()
     except:
@@ -113,23 +150,26 @@ class music_updated(commands.Cog):
   ## display queue
   @commands.command()
   async def queue(self,ctx):
+    global queue
+    global nowPlaying
     message = []
     i = 1
-    for item in self.song_list:
+    for item in queue[ctx.guild.id]:
       message.append(str(i)+". "+item.split("#+-+#")[0])
       i+=1
     
     message_string = "\n".join(message)
-    if self.now_playing == None:
+    if nowPlaying[ctx.guild.id] == None:
       now_playing = "-"
     else:
-      now_playing = self.now_playing
+      now_playing = nowPlaying[ctx.guild.id]
     await ctx.send("Now Playing: "+now_playing+"\nCurrent Queue:\n"+message_string)
     
   ## remove song from queue
   @commands.command()
   async def remove(self,ctx,ind = "-1"):
-    if len(self.song_list) == 0:
+    global queue
+    if len(queue[ctx.guild.id]) == 0:
       await ctx.send("The playlist is empty and there is nothing to remove ❗")
       return
 
@@ -139,29 +179,29 @@ class music_updated(commands.Cog):
 
     elif ind == "first":
       try:
-        removedVid = self.song_list[(0)].split("#+-+#")[0]
-        del self.song_list[(0)]
+        removedVid = queue[ctx.guild.id][0].split("#+-+#")[0]
+        del queue[ctx.guild.id][0]
         await ctx.send(removedVid + " has been removed from queue ❌")
       except Exception as e:
         print(e)
     
     elif ind == "last":
       try:
-        removedVid = self.song_list[(-1)].split("#+-+#")[0]
-        del self.song_list[(-1)]
+        removedVid = queue[ctx.guild.id][-1].split("#+-+#")[0]
+        del queue[ctx.guild.id][-1]
         await ctx.send(removedVid + " has been removed from queue ❌")
       except Exception as e:
         print(e)
     
     elif ind == "all":
-      self.song_list  = []
+      queue[ctx.guild.id]  = []
       await ctx.send("The playlist has been cleared ❌")
     
     else:
       try:
         ind = int(ind)
-        removedVid = self.song_list[(ind-1)].split("#+-+#")[0]
-        del self.song_list[(ind-1)]
+        removedVid = queue[ctx.guild.id][(ind-1)].split("#+-+#")[0]
+        del queue[ctx.guild.id][(ind-1)]
         await ctx.send(removedVid + " has been removed from queue ❌")
       except Exception as e:
         print(e)
@@ -169,6 +209,7 @@ class music_updated(commands.Cog):
   
   @commands.command()
   async def play(self, ctx, *,song = None):
+    global queue
     #print(song)
     target = song
     ## if song argument is None
@@ -192,28 +233,32 @@ class music_updated(commands.Cog):
         return
       target = url[0] ## get first result
     
-    print(target)
+    #print(target)
     ## first song
-    if self.now_playing is None:
-      self.now_playing = pafy.new(target).title
-      await self.play_song(ctx,target,self.now_playing)
+    if nowPlaying[ctx.guild.id] is None:
+      nowPlaying[ctx.guild.id] = pafy.new(target).title
+      await self.play_song(ctx,target,nowPlaying[ctx.guild.id])
       
     else:
       try:
         vidTitle = pafy.new(target).title
-        self.song_list.append(vidTitle+"#+-+#"+target)
+        queue[ctx.guild.id].append(vidTitle+"#+-+#"+target)
         await ctx.send(vidTitle + " is added into queue")
-      except:
+      except Exception as e:
+        print(queue.keys())
+        print(e)
         await ctx.send("This url has some issues 🚧 Please use the song name instead")
         
-      #print(self.song_list)
+      #print(queue)
 
   @commands.command()
   async def skip(self,ctx):
-    if len(self.song_list) > 0:
-      await ctx.send("Skipping to next song ⏭: "+self.song_list[0].split("#+-+#")[0])
+    global queue
+    print("skip fx")
+    if len(queue[ctx.guild.id]) > 0:
+      await ctx.send("Skipping to next song ⏭: "+queue[ctx.guild.id][0].split("#+-+#")[0])
       ctx.voice_client.stop()
-    elif len(self.song_list) == 0:
+    elif len(queue[ctx.guild.id]) == 0:
       ctx.voice_client.stop()
       await ctx.send("Player has reached the end of playlist 🔚")
     else:
@@ -224,6 +269,9 @@ class music_updated(commands.Cog):
     message_string = '\n'.join(command_list)
     await ctx.send("Help: \n"+message_string)
 
+  @commands.command()
+  async def loop(self,ctx):
+    pass
 
-def setup(client):
-  client.add_cog(music_updated(client))
+
+
