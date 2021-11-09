8c8f8f4ddb49d0cb2ef4ddaaf5e5df2747140c4e
Programmer420-1 <limweixin17@gmail.com>
2021-11-09 09:19:53 +0800

    Remove redundancy

--- Discord OnBot/main.py
+++ Discord OnBot/main.py
@@ -1,7 +1,6 @@
 import discord
 from discord.ext import commands
 from music_updated import music_updated
-#from server import keep_alive
 
 cogs  = [music_updated]
 
@@ -17,5 +16,4 @@ async def setup(): # wait the bot is ready only we add the cog , otherwise self.
   client.add_cog(music_updated(client))
 
 client.loop.create_task(setup())
-#keep_alive()
 client.run("ODk0MDkxMTQ5MDc2MjA1NTg4.YVk9RQ.jB2mmKt3S4E8H7AMNMxlWEHvGD0")
